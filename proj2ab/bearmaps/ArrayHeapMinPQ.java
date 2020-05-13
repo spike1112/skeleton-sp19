05cc05fda5bb2286ef8455da7aaac1fd1bb4ebb4
spike1112 <ovozlj@pm.me>
2020-05-13 13:43:38 +0800

    add ArrayHeapMinPQ.java

---
+++ proj2ab/bearmaps/ArrayHeapMinPQ.java
@@ -0,0 +1,135 @@
+package bearmaps;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T>{
+
+    private ArrayList<Node<T>> heap;
+    private Map<T, Integer> map;
+
+    private static class Node<T> implements Comparable<Node<T>> {
+        T item;
+        double priority;
+
+        private Node(T i, double p) {
+            item = i;
+            priority = p;
+        }
+
+        @Override
+        public int compareTo(Node other) {
+            return Double.compare(priority, other.priority);
+        }
+
+        public T getItem() {
+            return item;
+        }
+
+        public double getPriority() {
+            return priority;
+        }
+
+        public void setItem(T item) {
+            this.item = item;
+        }
+
+        public void setPriority(double priority) {
+            this.priority = priority;
+        }
+    }
+
+    public ArrayHeapMinPQ() {
+        heap = new ArrayList<>();
+        heap.add(null);
+        map = new HashMap<>();
+    }
+
+
+
+    @Override
+    public void add(T item, double priority) {
+        if (map.containsKey(item)) {
+            throw new IllegalArgumentException();
+        }
+
+        heap.add(new Node<>(item, priority));
+        map.put(item, size());
+        swim(size());
+
+    }
+
+    private void swim(int k) {
+        while (k > 1 && greater(parent(k), k)) {
+            swap(k, parent(k));
+            k = k / 2;
+        }
+    }
+
+    private boolean greater(int i, int j) {
+        return heap.get(i).compareTo(heap.get(j)) > 0;
+    }
+
+    private void swap(int i, int j) {
+        T tempT = heap.get(i).getItem();
+        double tempP = heap.get(i).getPriority();
+        Node<T> node1 = heap.get(i);
+        Node<T> node2 = heap.get(j);
+        node1.setItem(node2.getItem());
+        node1.setPriority(node2.getPriority());
+        node2.setItem(tempT);
+        node2.setPriority(tempP);
+        map.put(node1.getItem(), i);
+        map.put(node2.getItem(), j);
+    }
+
+    private int parent(int k)  {
+        return k / 2;
+    }
+
+    @Override
+    public boolean contains(T item) {
+        return map.containsKey(item);
+    }
+
+    @Override
+    public T getSmallest() {
+        return heap.get(1).item;
+    }
+
+    @Override
+    public T removeSmallest() {
+        swap(1, size());
+        Node<T> removed = heap.remove(size());
+        sink(1);
+        return removed.getItem();
+    }
+
+    void sink(int k) {
+        while (2 * k <= size()) {
+            int j = 2 * k;
+            if (j < size() && greater(j, j + 1)) {
+                j = j + 1;
+            }
+            if (!greater(k, j)) {
+                break;
+            }
+            swap(k, j);
+            k = j;
+        }
+    }
+
+    @Override
+    public int size() {
+        return heap.size() - 1;
+    }
+
+    @Override
+    public void changePriority(T item, double priority) {
+        int index= map.get(item);
+        heap.get(index).setPriority(priority);
+        sink(index);
+        swim(index);
+    }
+}
--- proj2ab/bearmaps/ArrayHeapMinPQTest.java
+++ proj2ab/bearmaps/ArrayHeapMinPQTest.java
@@ -2,6 +2,26 @@ package bearmaps;
 
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class ArrayHeapMinPQTest {
+
+    //simple test for my ArrayHeapMinPQTest
+    @Test
+    public void testMinPQ() {
+        ArrayHeapMinPQ<Integer> heap = new ArrayHeapMinPQ<>();
+        heap.add(23, 1.05);
+        heap.add(45, 0.05);
+        heap.add(1, 13.0);
+        heap.add(65, 3.4);
+        assertEquals(45, heap.getSmallest(), 0);
+        heap.removeSmallest();
+        assertEquals(23, heap.getSmallest(), 0);
+        assertEquals(3, heap.size());
+        heap.changePriority(65, 0.001);
+        heap.add(34, 3.5);
+        heap.add(56, 0.3);
+        assertTrue(heap.contains(34));
+        assertEquals(65, heap.getSmallest(), 0);
+    }
 }
